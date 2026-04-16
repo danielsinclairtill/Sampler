2aea7a1a15cc4dda3c1b276ab5ba7a78a7c37a20
Daniel Till <daniel.sinclair.till@gmail.com>
2026-04-16 12:15:59 -0400

    update itemdetailview

--- SwiftUI-MVVM-Concurrency/Sampler/Sampler/View/ItemDetail/ItemDetailView.swift
+++ SwiftUI-MVVM-Concurrency/Sampler/Sampler/View/ItemDetail/ItemDetailView.swift
@@ -16,90 +16,86 @@ struct ItemDetailView: View {
     }
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 16) {
-                // Item Image
-                AsyncImageView(
-                    url: viewModel.output.item?.image,
+        VStack(alignment: .leading, spacing: 16) {
+            // Item Image
+            AsyncImageView(
+                url: viewModel.output.item?.image,
+            )
+            .frame(width: 240, height: 240)
+            .cornerRadius(12)
+            .clipped()
+            
+            // Item Title
+            Text(viewModel.output.item?.name ?? " ")
+                .font(.title)
+                .fontWeight(.bold)
+                .lineLimit(2)
+            
+            // Author Info
+            HStack(spacing: 12) {
+                CircularImageView(
+                    url: viewModel.output.item?.user?.image,
+                    placeholder: Image("UnkownUser"),
+                    size: 40
                 )
-                .frame(width: 240, height: 240)
-                .cornerRadius(12)
-                .clipped()
                 
-                // Item Title
-                Text(viewModel.output.item?.name ?? "")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .lineLimit(2)
-                
-                // Author Info
-                HStack(spacing: 12) {
-                    CircularImageView(
-                        url: viewModel.output.item?.user?.image,
-                        placeholder: Image("UnkownUser"),
-                        size: 40
-                    )
-                    
-                    VStack(alignment: .leading) {
-                        Text(viewModel.output.item?.user?.username ?? "")
-                            .font(.subheadline)
-                            .fontWeight(.medium)
-                            .lineLimit(1)
-                    }
-                    Spacer()
-                    Button(action: {
-                        viewModel.tappedLikeButton()
-                    }) {
-                        Image(systemName: viewModel.output.isLiked ? "heart.fill" : "heart")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30)
-                    }
+                VStack(alignment: .leading) {
+                    Text(viewModel.output.item?.user?.username ?? " ")
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                        .lineLimit(1)
                 }
-                .padding(.vertical, 8)
-                
-                // Description
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(viewModel.output.item?.ingredients?.joined(separator: ", ") ?? "")
-                        .font(.body)
-                        .foregroundColor(.gray)
-                }
-                
                 Spacer()
-                
-                // Action Buttons
-                HStack(spacing: 12) {
-                    Button(action: {
-                        Task {
-                            await viewModel.tappedPostButton()
-                        }
-                    }) {
-                        Text("com.danielsinclairtill.Sampler.itemDetail.postButton.title")
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical, 12)
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-                    
-                    Button(action: {
-                        Task {
-                            await viewModel.tappedSaveButton()
-                        }
-                    }) {
-                        Text(viewModel.output.isSaved ? "com.danielsinclairtill.Sampler.itemDetail.saveButton.title.saved" : "com.danielsinclairtill.Sampler.itemDetail.saveButton.title.save")
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical, 12)
-                            .background(viewModel.output.isSaved ? Color.green : Color.gray)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-                    .disabled(viewModel.output.isSaved || viewModel.output.isSaving)
+                Button(action: {
+                    viewModel.tappedLikeButton()
+                }) {
+                    Image(systemName: viewModel.output.isLiked ? "heart.fill" : "heart")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30)
                 }
             }
-            .padding(.horizontal, 16)
-            .padding(.vertical, 12)
+            .padding(.vertical, 8)
+            
+            // Description
+            Text(viewModel.output.item?.ingredients?.joined(separator: ", ") ?? "")
+                .font(.body)
+                .foregroundColor(.gray)
+            
+            Spacer()
+            
+            // Action Buttons
+            HStack(spacing: 12) {
+                Button {
+                    Task {
+                        await viewModel.tappedPostButton()
+                    }
+                } label: {
+                    Text("com.danielsinclairtill.Sampler.itemDetail.postButton.title")
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 12)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                
+                Button {
+                    Task {
+                        await viewModel.tappedSaveButton()
+                    }
+                } label: {
+                    Text(viewModel.output.isSaved ? "com.danielsinclairtill.Sampler.itemDetail.saveButton.title.saved" : "com.danielsinclairtill.Sampler.itemDetail.saveButton.title.save")
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 12)
+                        .background(viewModel.output.isSaved ? Color.green : Color.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .disabled(viewModel.output.isSaved || viewModel.output.isSaving)
+            }
         }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 12)
         .onAppearOnce {
             Task {
                 await viewModel.viewDidLoad()
@@ -119,6 +115,24 @@ struct ItemDetailPreview: View {
 }
 
 #Preview("Blank") {
+    ItemDetailPreview(
+        output: .init(
+            item: .init(
+                name: nil,
+                ingredients: [],
+                difficulty: nil,
+                tags: [],
+                image: nil
+            ),
+            isSaved: false,
+            isSaving: false,
+            isLiked: false
+        )
+    )
+}
+
+
+#Preview("Standard") {
     ItemDetailPreview(
         output: .init(
             item: .init(
